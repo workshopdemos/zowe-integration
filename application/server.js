22bf4382fb79add22de9e47f2b92676567f8cf6d
Sergei Kurnevich <sergei.kurnevich@broadcom.com>
2025-03-11 21:27:19 +0100

    Initial

---
+++ application/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+
+const scheme = "https";
+const host = "";
+const port = "7554";
+
+const service1 = `${scheme}://${host}:${port}/gateway/api/v1/version`;
+const service2 = `${scheme}://${host}:${port}/ibmzosmf/api/v1/zosmf/info`;
+
+const app = express();
+const app_port = 8080;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Get auth token // "Authorization": "Basic ",
+const service2_headers = {
+    "X-CSRF-ZOSMF-HEADER": "",
+    "accept": "application/json"
+}
+
+app.get('/api/combined', async (req, res) => {
+    try {
+        const [res1, res2] = await Promise.all([
+            axios.get(service1),
+            axios.get(service2, { headers: service2_headers } )
+        ]);
+
+        // console.log('status', res2.status);
+        // console.log('headers', res2.headers);
+        // console.log('body', JSON.stringify(res2.body));
+
+        res.json({service1: res1.data, service2: res2.data});
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch data'});
+    }
+});
+
+app.listen(app_port, () => {
+    console.log(`Server is running at http://localhost:${app_port}`)
+});
